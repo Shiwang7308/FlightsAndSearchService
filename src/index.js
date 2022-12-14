e15d6f610ba478af9be37c8f48a6a8b090b7c673
Shiwang Gupta <guptashiwang02@gmail.com>
2022-12-14 23:28:56 +0530

    body parser middle ware addition in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig')
 
@@ -7,6 +8,9 @@ const setupAndStartServer = async()=>{
     // create the express object
    const app = express();
 
+   // sertup middleware 
+   app.use(bodyParser.json());
+   app.use(bodyParser.urlencoded({extended: true}));
    app.listen(PORT,()=>{
     
     console.log(`Server started at ${PORT}`);
