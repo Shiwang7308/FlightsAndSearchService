f000ad16428afc674c5cc5aa5d372340c5508eea
Shiwang Gupta <guptashiwang02@gmail.com>
2022-12-16 19:18:41 +0530

    Completed City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,12 +1,17 @@
 const {City} = require('../models/index.js')
 
 class CityRepository{
+    
     async createCity({name}) {
         try{
-              const city = await City.create({name});
+              const city = await City.create({
+                // name:name
+                name
+            });
               return city;
         }
-        catch{
+        catch(error){
+            console.log("Something went wrong in the repository level")
             throw {error};
         }
     }
@@ -17,9 +22,35 @@ class CityRepository{
                 where: {
                     id:cityId
                 }
-            })
+            });
+            return true;
         }
-        catch{
+        catch(error){
+            console.log("Something went wrong in the repository level")
+            throw {error}; 
+        }
+    }
+
+    async updateCity(cityId,data){
+             try {
+                   const city = await City.update(data,{
+                    where:{
+                        id:cityId
+                    }
+                   });
+             } catch (error) {
+                console.log("Something went wrong in the repository level")
+                throw {error}; 
+             }
+    }
+
+    async getCity(cityId) {
+        try {
+            
+            const city = await City.findByPk(cityId)
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository level")
             throw {error}; 
         }
     }
